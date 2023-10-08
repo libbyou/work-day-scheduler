0c48cf430799c23cbaeddf93e997bd9f309060a5
libbyou <lcyou95@gmail.com>
2023-10-07 19:31:15 -0500

    localStorag and init timeblocks

--- script.js
+++ script.js
@@ -1,22 +1,83 @@
 var dateDisplayEl = $("#current-day");
-var timeBlock = $(".time-block")
-// var eventDescriptionInputEl = $(".description")
-var saveButton = $(".saveBtn")
+var timeBlock = $(".time-block");
+// var eventDescriptionInputEl = $(".description");
+var saveButton = $('.saveBtn');
+var todayDate = dayjs().format('dddd, MMMM D');
+var nowHour = dayjs().format('h');
+// console.log(nowTime);
+// var currentHour = $('div').id
+// console.log($('div'))
 
 function displayDay() {
-  var todayDate = dayjs().format('dddd, MMMM D');
   dateDisplayEl.text(todayDate);
 }
 
 displayDay();
 
+function init() {
+  var localStorageObj = localStorage
+  var arryLocalStorageObj = Object.entries(localStorageObj) // return array of object
+
+  for (var i = 0; i < arryLocalStorageObj.length; i++) {
+    var localStoragePair = arryLocalStorageObj[i]
+    var key = localStoragePair[0]
+    var value = localStoragePair[1]
+
+    //TODO check if jqeury found element by key, if NOT skip
+    // TODO potential refactor to loop elemtns ONCE and fetch existing data
+    if (key) {
+      $('#' + key).children(".description").val(value)
+    }
+  }
+
+  var listOfParentContainer = $("div[id^=hour-")
+
+  for (var i = 0; i < listOfParentContainer.length; i++) {
+    // console.log(listOfParentContainer[i].id, nowHour)
+    var parent = listOfParentContainer[i]
+    // console.log(parent)
+    var replacedNum = listOfParentContainer[i].id.replace('hour-', "")
+    var eventDescriptionInputEl = $("#" + listOfParentContainer[i].id).children("textarea.description")
+    if (replacedNum == nowHour) {
+      eventDescriptionInputEl.addClass('present')
+    } else if (replacedNum > nowHour) {
+      eventDescriptionInputEl.addClass('future')
+    } else {
+      eventDescriptionInputEl.addClass('past')
+    }
+  }
+
+
+  // for (var [key, value] of Object.entries(localStorage)) {
+  //   console.log(`${key}: ${value}`);
+  // }
+  // var storedEvents = localStorage.getItem("event");
+  // var testData = { 'hour-9': "this is some text", "hour-10": 'this is some other text', "hour-11": 'this is more text', "hour-9": "this is more" }
+  // console.log(storedEvents);
+  // if (storedEvents !== null) {
+  //   $("<textarea>").text(storedEvents);
+  // }
+}
+
 function saveEvent(event) {
   event.preventDefault();
-  var eventDescription = document.querySelector(".description").value;
-  localStorage.setItem("event", eventDescription);
+  var parentContainerID = $(event.target).parent()[0].id
+  var parentContainer = $(event.target).parent()
+  var eventDescriptionInputEl = parentContainer.children('textarea').val();
+  if (parentContainerID) {
+    localStorage.setItem(parentContainerID, eventDescriptionInputEl);
+  }
+  // var btnId = event.target.id
+  // var timeBlockId = btnId.replace("-button", "");
+  // console.log("Testarea", $(timeBlockId + '+ description'))
+  // var eventDescription = $(timeBlockId + '+ description').val() // "hour-9 + descrition"
+  // console.log(eventDescription);
+  // localStorage.setItem("event", eventDescription);
 }
 
 saveButton.on('click', saveEvent);
+
+init();
 
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
@@ -42,3 +103,28 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+function changeColor() {
+  var currentHour = $('div');
+  consolelog(currentHour)
+}
+// if (currentHour == nowTime) {
+//   eventDescriptionInputEl.attr('present')
+// }; else if (currentHour > nowTime) {
+//   eventDescriptionInputEl.attr('future')
+// }; else (currentHour < nowTime) {
+//   eventDescriptionInputEl.attr('past')
+// };
+// }
+
+// var findElement = $(".test")
+// if(findElement.length <= 0) {
+//   return
+// }
+
+//logic to update DOM
+
+// find each parent id
+// get current hour from time
+//compare parent id with out prefix to current hour
+// inject css base on result
